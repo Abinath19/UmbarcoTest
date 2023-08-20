5e0a89dd016a89c3fb6f24e9274014a82b6a44cf
Abinath Mukunthan <Abinath Mukunthan@DESKTOP-AVL4CS2>
2023-08-20 16:07:56 +0400

    Add project files.

---
+++ UmbarcoTest/Controller/SendMailController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Web;
+using System.Web.Mvc;
+using UmbarcoTest.Models;
+using Umbraco.Web.Mvc;
+
+namespace UmbarcoTest.Controller
+{
+    public class SendMailController : SurfaceController
+    {
+        public const string ViweFolder = "~/Views/Partials/Contact/";
+
+        public ActionResult Index()
+        {
+            return PartialView(ViweFolder+"_SendMail.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Save(SendMail model)
+        {
+            if (ModelState.IsValid)
+            {
+                Mail(model);
+                return RedirectToCurrentUmbracoPage();
+            }
+            return CurrentUmbracoPage();
+        }
+
+        private void Mail(SendMail model)
+        {
+            MailMessage msg = new MailMessage(model.Email, "abinathmukunthan@gmail.com");
+            msg.Subject = "Umbraco Cold Banana Test Mail";
+            msg.Body = model.Message;
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(msg);
+
+        }
+    }
+}
---
+++ UmbarcoTest/Models/SendMail.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace UmbarcoTest.Models
+{
+    public class SendMail
+    {
+        [Required]
+        [Display(Name ="First Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [Display(Name = "Message")]
+        public string Message { get; set; }
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
